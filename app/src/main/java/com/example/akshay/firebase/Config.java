b47ef6bd8c222aafaa705905bfd1c40843a23937
Akshay <akshayyadav7613@gmail.com>
2019-02-03 21:28:40 +0530

    add user data to firebase client

---
+++ app/src/main/java/com/example/akshay/firebase/Config.java
@@ -0,0 +1,9 @@
+package com.example.akshay.firebase;
+
+import java.net.URI;
+
+public class Config
+{
+
+    public static String url ="https://console.firebase.google.com/project/fir-9a1fe/database/firestore/data/user/v068c9lymGyMQ9FG6mCO";
+}
--- app/src/main/java/com/example/akshay/firebase/MainActivity.java
+++ app/src/main/java/com/example/akshay/firebase/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.akshay.firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText t1,t2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        t1=findViewById(R.id.textView1);
+        t2=findViewById(R.id.textView2);
+    }
+
+    public void click(View view)
+    {
+   String name = t1.getText().toString();
+   String address = t2.getText().toString();
+   if(name.equals("")| address.equals(""))
+   {
+       Toast.makeText(this, "enter the values", Toast.LENGTH_SHORT).show();
+   }
 
     }
 }
